af2193ab06cc808af494ae85d0e76bc2b2461198
AdrianCraft07 <proadrian684@gmail.com>
2022-09-27 21:53:36 -0500

    Update Object.js

--- extension/Object.js
+++ extension/Object.js
@@ -1,6 +1,25 @@
 "use strict";
-
-const Json = include("https://raw.githubusercontent.com/AdrianCraft07/CDN/main/classes/Json.js")
+class Json {
+    #object;
+    constructor(obj = {}) {
+        if (typeof obj == 'object')
+            this.#object = JSON.parse(JSON.stringify(obj));
+        else
+            throw TypeError(`"${obj}" not is a json valid`);
+    }
+    toObject() {
+        return this.#object;
+    }
+    toString() {
+        return JSON.stringify(this.#object, null, 2);
+    }
+    compare(Json) {
+        return Json.toObject().compare(this.#object);
+    }
+    copy() {
+        return new Json(this.toObject());
+    }
+}
 function type(data) {
     return toString.call(data);
 }
